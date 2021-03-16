364504371b040c8c75fa2ec972a341b9578618d8
jsmithb117 <jsmithb117@gmail.com>
2021-03-16 12:53:10 -0700

    Server structure complete

---
+++ server/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/:courseNumber', (req, res) => {
+  res.sendFile(path.resolve('./public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server listening at http://localhost:${port}`);
+});
