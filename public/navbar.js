b7a95e7b99395e753fedea748432c3ac085267c1
jsmithb117 <jsmithb117@gmail.com>
2021-03-18 15:45:46 -0700

    Cleaned up code, added comments

--- public/navbar.js
+++ public/navbar.js
@@ -1,28 +1,19 @@
-/* eslint-disable func-names */
-/* eslint-disable func-style */
 /* eslint-disable no-undef */
-/* eslint-disable no-console */
-console.log('navbar loaded');
-
-window.onscroll = function () { grabNavbar(); };
-
 const navbar = document.getElementById('navbar');
-let sticky = navbar.offsetTop;
-let once = true;
+let sticky;
+let gotOffset = false;
 
-function grabNavbar() {
-  console.log('window.pageYOffset: ', window.pageYOffset);
-  console.log('sticky: ', sticky);
+const grabNavbar = () => {
   const offset = window.pageYOffset;
-  if (once) {
+  if (!gotOffset) {
     sticky = navbar.offsetTop;
-    once = false;
+    gotOffset = true;
   }
-  console.log(offset >= sticky);
-
   if (offset >= sticky) {
     navbar.classList.add('sticky');
   } else {
     navbar.classList.remove('sticky');
   }
-}
+};
+
+window.onscroll = () => { grabNavbar(); };
